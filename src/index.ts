e6102ded4ff720ebc61b5e125df13e7ce76c7b81
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-12-11 01:46:28 +0800

    added bulk component

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Button from './components/Button';
 import ButtonMutate from './components/ButtonMutate';
 import EmptyState from './components/EmptyState';
+import Bulk from './components/Bulk';
 
 export {
     Button,
     ButtonMutate,
-    EmptyState
+    EmptyState,
+    Bulk
 }
