571891db451726af5c82eae51c6a0b2cf5f02d86
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-12-11 05:15:15 +0800

    breadCrumb

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import Button from './components/Button';
 import ButtonMutate from './components/ButtonMutate';
 import EmptyState from './components/EmptyState';
 import Bulk from './components/Bulk';
+import BreadCrumb from './components/breadcrumb/BreadCrumb'
 
 export {
     Button,
     ButtonMutate,
     EmptyState,
-    Bulk
+    Bulk,
+    BreadCrumb
 }
