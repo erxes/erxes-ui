135b95fa876bc6beb19b30706acfb710b06f3195
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-12-11 06:35:41 +0800

    add common styles chooser

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ import EmptyState from './components/EmptyState';
 import Filter from './components/filter/Filter';
 import FilterableList from './components/filterableList/FilterableList';
 import FilterableListStyles from './components/filterableList/styles';
+import {
+    ActionTop as ChooserStyleActionTop, Column as ChooserStyleColumn,
+    Columns as ChooserStyleColumns, Footer as ChooserStyleFooter, Select as ChooserStyleSelect,
+    Title as ChooserStyleTitle
+} from './styles/chooser';
 
 export {
   BreadCrumb,
@@ -18,4 +23,10 @@ export {
   Filter,
   FilterableList,
   FilterableListStyles,
+  ChooserStyleColumns,
+  ChooserStyleColumn,
+  ChooserStyleTitle,
+  ChooserStyleFooter,
+  ChooserStyleSelect,
+  ChooserStyleActionTop,
 }
