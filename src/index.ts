2a9d19caf39b31702c0f9b42e42b54d06dd46fbe
BatAmar Battulga <batamar.b@nmtec.co>
2020-12-11 01:17:36 +0800

    added empty state

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Button from './components/Button';
 import ButtonMutate from './components/ButtonMutate';
+import EmptyState from './components/EmptyState';
 
 export {
     Button,
-    ButtonMutate
+    ButtonMutate,
+    EmptyState
 }
