5d0be245e81bc59e003c628a5f5aa9934da3d5bf
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-12-11 05:24:26 +0800

    add emptyContent

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+import BreadCrumb from './components/breadcrumb/BreadCrumb';
+import Bulk from './components/Bulk';
 import Button from './components/Button';
 import ButtonMutate from './components/ButtonMutate';
+import EmptyContent from './components/empty/EmptyContent';
 import EmptyState from './components/EmptyState';
-import Bulk from './components/Bulk';
-import BreadCrumb from './components/breadcrumb/BreadCrumb'
 
 export {
+    BreadCrumb,
+    Bulk,
     Button,
     ButtonMutate,
+    EmptyContent,
     EmptyState,
-    Bulk,
-    BreadCrumb
 }
