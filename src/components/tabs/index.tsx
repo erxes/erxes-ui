240b7ceb60ac177ba1bb60378cc9ecb767f699cf
bilguun1324 <bilguun1324@gmail.com>
2021-08-02 12:50:26 +0800

    updated tabs

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import { TabCaption, TabContainer } from './styles';
+import React, { useState } from 'react';
+import { TabCaption, TabContainer, Slider, Listener, Line } from './styles';
 
-function Tabs(props: {
-  children: React.ReactNode;
+type Props = {
+  children: any;
+  tabNumber?: number;
   grayBorder?: boolean;
   full?: boolean;
   vertical?: boolean;
-}) {
-  return <TabContainer {...props} />;
-}
+  backgroundFull?: boolean;
+  color?: string;
+  textColor?: string;
+};
 
 type TabTitleProps = {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
-  backgroundColor?: string;
-  textColor?: string;
-  type?: 'underlined' | 'underlined-vertical';
 };
 
-function TabTitle(props: TabTitleProps) {
-  return <TabCaption {...props} />;
+const TabTitle: React.FC<TabTitleProps> = (props) => {
+  return (
+    <TabCaption {...props} />
+  )
+}
+
+const Tabs: React.FC<Props> = (props) => {
+  const [linePosition, setLinePosition] = useState(0);
+  const childRef = React.useRef<HTMLHeadingElement>(null);
+  const { children, grayBorder, full, vertical, backgroundFull, color, textColor } = props;
+
+  const onClick = (index: number) => {
+    setLinePosition(index)
+  }
+
+  const newChildren = children.map((child: any, index: number) => {
+    return <Listener onClick={() => onClick(index)} textColor={textColor}><div ref={childRef}>{child}</div></Listener>
+  })
+
+  return (
+    <TabContainer grayBorder={grayBorder} full={full} vertical={vertical}>
+      {newChildren}
+      <Slider
+        vertical={vertical}
+        sizes={`${100 / children.length}%`}
+        position={linePosition}
+        backgroundFull={backgroundFull}
+      >
+        <Line
+          vertical={vertical}
+          backgroundFull={backgroundFull}
+          color={color}
+          sizes={childRef.current && (vertical ? childRef.current.clientHeight : childRef.current.clientWidth)}
+        />
+      </Slider>
+    </TabContainer>
+  )
 }
 
 export { Tabs, TabTitle };
--- src/components/tabs/styles.ts
+++ src/components/tabs/styles.ts
@@ -12,54 +12,73 @@ const TabContainer = styledTS<{ grayBorder?: boolean; full?: boolean, vertical?:
   z-index: 2;
   display: flex;
   flex-direction: ${props => props.vertical && 'column'};
-  justify-content: ${props => props.full && 'space-evenly'};
+  justify-content: ${props => props.full && 'space-around'};
   flex-shrink: 0;
   height: ${props => props.vertical ? 'auto' : `${dimensions.headerSpacing}px`};
 `;
 
-const TabCaption = styledTS<{ type?: 'underlined' | 'underlined-vertical', backgroundColor?: string, textColor?: string }>(styled.span)`
+const TabCaption = styled.span`
   cursor: pointer;
   display: inline-block;
-  color: ${colors.textSecondary};
-  font-weight: ${typography.fontWeightRegular};
   padding: 15px ${dimensions.coreSpacing}px;
   position: relative;
-  transition: all ease 0.3s;
-  background-color: ${props => props.backgroundColor}
-
-  &:hover {
-    color: ${colors.textPrimary};
-  }
 
   i {
     margin-right: 3px;
   }
-
-  &.active {
-    color: ${props => props.textColor ? props.textColor : colors.textPrimary};
-    font-weight: 500;
-
-    &:before {
-      ${props => (props.type === 'underlined' || props.type === undefined) && `
-      border-bottom: 3px solid ${colors.colorSecondary};
-      content: '';
-      width: 100%;
-      position: absolute;
-      z-index: 1;
-      left: 0;
-      bottom: -1px;
-      `}
-      ${props => props.type === 'underlined-vertical' && `
-      border-right: 3px solid ${colors.colorSecondary};
-      content: '';
-      height: 100%;
-      position: absolute;
-      z-index: 1;
-      right: -1px;
-      top: 0px;
-      `}
-    }
-  }
 `;
 
-export { TabContainer, TabCaption };
+const Slider = styledTS<{ sizes?: string, position?: number, vertical?: boolean, backgroundFull?: boolean }>(styled.div)`
+  ${props => props.vertical ? `
+    height: ${props.sizes};
+    right: -1px;
+    top: 0px;
+    transform: translateY(${props.position && 100 * props.position}%);
+    ${props.backgroundFull && 'width: 100%'};
+  ` : `
+    width: ${props.sizes};
+    left: 0;
+    bottom: -1px;
+    transform: translateX(${props.position && 100 * props.position}%);
+    ${props.backgroundFull && 'height: 100%'};
+  `}
+  transition: 0.25s ease;
+  position: absolute;
+  content: '';
+  z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Listener = styledTS<{ textColor?: string, ref?: any }>(styled.div)`
+  width: fit-content;
+  color: ${props => props.textColor ? props.textColor : colors.textSecondary};
+  font-weight: ${typography.fontWeightRegular};
+  &:hover {
+    font-weight: 500;
+    color: ${props => props.textColor ? props.textColor : colors.textPrimary};
+  }
+`
+
+const Line = styledTS<{ sizes?: any, vertical?: boolean, backgroundFull?: boolean, color?: string }>(styled.div)`
+  ${props => props.vertical ? `
+    height: ${props.sizes ? `${props.sizes}px` : `100%`};
+    ${props.backgroundFull ? `
+      background: ${props.color ? props.color : colors.colorSecondary};
+      width: 100%;
+    ` : `
+      border-right: 3px solid ${props.color ? props.color : colors.colorSecondary};
+    `}
+  ` : `
+    width: ${props.sizes ? `${props.sizes}px` : `100%`};
+    ${props.backgroundFull ? `
+      background: ${props.color ? props.color : colors.colorSecondary};
+      height: 100%;
+    ` : `
+    border-bottom: 3px solid ${props.color ? props.color : colors.colorSecondary};
+    `}
+  `}
+`
+
+export { TabContainer, TabCaption, Slider, Listener, Line };
